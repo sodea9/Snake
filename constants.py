60f4b07a140c780debed6e58f1216acecaf60b49
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2023-01-23 00:01:00 -0500

    constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+import pygame as pg
+
+SQ = 20
+GRIDSIZE = 30
+SCREEN_WIDTH = (SQ+1) * GRIDSIZE
+SCREEN_HEIGHT = SCREEN_WIDTH
+GAMESPEED = 12
+
+BLACK = pg.Color(0,0,0)
+WHITE = pg.Color(255,255,255)
+RED = pg.Color(255,0,0)
+GREEN = pg.Color(0,255,0)
